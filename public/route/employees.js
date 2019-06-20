a26734eac54dad33039f3153cdf6f806eb3194ac
PabloWinter <pablowinter7@hotmail.com>
2019-06-20 17:22:47 -0600

    Added DELETE route to delete existing employees from database

--- public/route/employees.js
+++ public/route/employees.js
@@ -28,7 +28,6 @@ router.get('/:id', (req, res) => {
     });
 });
 
-
 //Insert a new record in the database
 router.post('/', (req, res) => {
     //Getting the parameters from the body to insert a new record
@@ -40,6 +39,37 @@ router.post('/', (req, res) => {
         else{
             res.json(
                 {Status: 'Record has been added'}
+            );
+        }
+    });
+});
+
+//Update existing record
+router.put('/:id', (req,res) => {
+    let { name, salary } = req.body;
+    let id = req.params.id;
+    dbConnection.query('UPDATE Employees SET name = ?, salary = ? WHERE id = ?', [name, salary, id], (error, rows) => {
+        if(error){
+            console.log(error);
+        }
+        else{
+            res.json(
+                {Status: 'Record has been updated'}
+            );
+        }
+    });    
+});
+
+//Delete a record from the database
+router.delete('/:id', (req, res) => {
+    let id = req.params.id;
+    dbConnection.query('DELETE FROM Employees WHERE id = ?', [id], (error, rows) => {
+        if(error){
+            console.log(error);
+        }
+        else{
+            res.json(
+                {Status: 'Employee has been deleted'}
             );
         }
     });
